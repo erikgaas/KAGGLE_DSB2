66d687c224ec6fa4033c331232a9317851214653
Erik Gaasedelen <erikgaas@gmail.com>
2016-08-26 11:37:26 -0500

    get rid of bad image assignments

--- find_ventricle_location.py
+++ find_ventricle_location.py
@@ -237,10 +237,10 @@ def draw_center_for_check(dcm_path, id, sax, point, points):
         os.mkdir(debug_folder)
     ds = dicom.read_file(dcm_path)
     img = convert_to_grayscale_with_increase_brightness_fast(ds.pixel_array, 1)
-    print(type(img))
     cv2.circle(img, (int(round(point[1], 0)), int(round(point[0], 0))), 5, 255, 3)
-    img = cv2.line(img, (points[1], points[0]), (points[3], points[2]), 127, thickness=2)
-    img = cv2.line(img, (points[5], points[4]), (points[7], points[6]), 127, thickness=2)
+
+    cv2.line(img, (points[1], points[0]), (points[3], points[2]), 127, thickness=2)
+    cv2.line(img, (points[5], points[4]), (points[7], points[6]), 127, thickness=2)
     # show_image(img)
     cv2.imwrite(os.path.join(debug_folder, str(id) + '_' + sax + '.jpg'), img)
 
