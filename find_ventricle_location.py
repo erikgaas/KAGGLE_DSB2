cc9f2d4f814792d74ba3ce0535acd2719f1cf16b
Erik Gaasedelen <erikgaas@gmail.com>
2016-08-26 14:35:03 -0500

    pen and paper for sanity

--- find_ventricle_location.py
+++ find_ventricle_location.py
@@ -285,11 +285,11 @@ def draw_center_for_check(dcm_path, id, sax, point, points, geom):
                             np.array([points[5], points[4]]),
                             np.array([points[7], points[6]]))
 
-    img = calculate_consistent_square(img, geom, center)
+    calculate_consistent_square(img, geom, center)
     #cv2.circle(img, (int(round(point[1], 0)), int(round(point[0], 0))), 5, 255, 3)
 
-    #cv2.line(img, (points[1], points[0]), (points[3], points[2]), 127, thickness=2)
-    #cv2.line(img, (points[5], points[4]), (points[7], points[6]), 127, thickness=2)
+    cv2.line(img, (points[1], points[0]), (points[3], points[2]), 127, thickness=2)
+    cv2.line(img, (points[5], points[4]), (points[7], points[6]), 127, thickness=2)
     # show_image(img)
     cv2.imwrite(os.path.join(debug_folder, str(id) + '_' + sax + '.jpg'), img)
 
